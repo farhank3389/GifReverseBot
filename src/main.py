a5ec41e0f575c593e20e83050d7f9678fd16caeb
Farhan <farhank3389@gmail.com>
2018-07-09 15:29:31 +0000

    added gfycat get token function. From https://github.com/gfycat/api_clients/blob/master/oauth/url_uploader/python/url_uploader.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 
 import config
 import praw
+import requests
 
 bot = praw.Reddit(
         user_agent    = "gifreversebot",
@@ -8,4 +9,20 @@ bot = praw.Reddit(
         client_secret = config.secret,
         username      = config.username,
         password      = config.password)
+
+
+def get_token(client_id, client_secret):
+        payload = {
+                'grant_type': 'client_credentials',
+                'client_id': client_id,
+                'client_secret': client_secret
+                }
+        req = requests.post('https://api.gfycat.com/v1/oauth/token', data=str(payload))
+        res = req.json()
+        if not 'access_token' in res:
+            print ('ERROR: Gfycat API is not available. Please try again later.')
+            sys.exit()
+        return res['access_token']
+
+
 
