a49e145a8cc30956763c86081e297ee079a42a37
Farhan <farhank3389@gmail.com>
2018-07-12 12:54:56 +0000

    check for zippy.gfycat.com and giant.gfycat.com

--- src/main.py
+++ src/main.py
@@ -49,7 +49,8 @@ def get_file(url):
         if parse.path.endswith('.gifv'):
             replace_gifv = parse._replace(path = parse.path[:-4] + 'mp4')
             url = replace_gifv.geturl()
-    else:
+    elif parse.netloc.lower() not in ("giant.gyfcat.com", "zippy.gfycat.com"):
+        print("Not imgur or gfycat link")
         return
 
     req = requests.get(url, stream=True)
